6dccf70e2767e6ad829e2fea484eaf9fcee43e65
napoleon101392 <napoleon101392@gmail.com>
2022-10-23 23:06:44 +0800

    renaming auth

--- router/auth.router.js
+++ router/auth.router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Authentication = require('../controllers/authenticate.controller'); 
+const Authentication = require('../controllers/authentication.controller'); 
 
 router.post('/login', Authentication.login);
 router.post('/logout', Authentication.logout);
